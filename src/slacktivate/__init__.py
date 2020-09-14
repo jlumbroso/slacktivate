94078dbc826fd6f44686130e803454c0d0285dcb
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-09-14 01:10:49 -0400

    Updated project information

--- src/slacktivate/__init__.py
+++ src/slacktivate/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.2.0'
+"""
+Slacktivate is a Python library and Command-Line Interface
+to assist in the provisioning of a Slack workspace, using
+both the Slack API and the Slack SCIM API.
+"""
+
+__version__ = "0.2.0"
