0b27cddebd9358e0c8b30c750a3edef7f7cbfe16
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-09-14 01:05:49 -0400

    Refactor of some methods

--- src/slacktivate/slack/methods.py
+++ src/slacktivate/slack/methods.py
@@ -3,6 +3,7 @@ import os
 import typing
 
 import slack
+import slack.errors
 import slack_scim
 
 import slacktivate.slack.classes
@@ -31,10 +32,11 @@ def user_patch(
     if user is None:
         return
 
-    result = slacktivate.slack.clients.scim().patch_user(
-        id=user.id,
-        user=changes,
-    )
+    with slacktivate.slack.clients.managed_scim() as scim:
+        result = scim.patch_user(
+            id=user.id,
+            user=changes,
+        )
 
     if result is not None:
         return slacktivate.slack.classes.to_slack_user(result)
@@ -60,3 +62,43 @@ def user_activate(user: slacktivate.slack.classes.SlackUserTypes) -> bool:
 def user_deactivate(user: slacktivate.slack.classes.SlackUserTypes) -> bool:
     return user_set_active(user=user, active=False)
 
+
+@slacktivate.slack.retry.slack_retry
+def list_custom_profile_fields(
+        index_by_label: bool = False,
+        silent_error: bool = True,
+) -> typing.Dict[str, str]:
+
+    # https://api.slack.com/methods/team.profile.get
+    try:
+        # this will handle standard errors
+        with slacktivate.slack.clients.managed_api() as api:
+            response = api.team_profile_get()
+
+        if not response.data["ok"]:
+            raise slack.errors.SlackApiError(
+                message="response failed",
+                response=response)
+
+    except slack.errors.SlackApiError as exc:
+        if silent_error:
+            # empty dictionary
+            return dict()
+        else:
+            raise
+
+    profile_fields = response.data.get("profile", dict()).get("fields")
+    if profile_fields is None:
+        if silent_error:
+            # empty dictionary
+            return dict()
+        else:
+            raise Exception("cannot find expected fields in response (`$.profile.fields`)")
+
+    index = "label" if index_by_label else "id"
+    indexed_fields = {
+        field[index]: field
+        for field in profile_fields
+    }
+
+    return indexed_fields
