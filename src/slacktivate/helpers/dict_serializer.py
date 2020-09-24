97bd836a2da3f62436d79b5a1e6297230c86eb78
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-09-24 00:05:28 -0400

    Added `to_flat_dict` and related to dict helpers

--- src/slacktivate/helpers/dict_serializer.py
+++ src/slacktivate/helpers/dict_serializer.py
@@ -6,6 +6,8 @@ __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
 
 __all__ = [
     "to_dict",
+    "to_flat_dict",
+    "dict_to_flat_dict",
 ]
 
 
@@ -35,3 +37,41 @@ def to_dict(
         }
 
     return to_dict(obj.__dict__)
+
+
+def dict_to_flat_dict(
+        dict_obj: typing.Dict[str, typing.Any],
+) -> typing.Dict[str, typing.Any]:
+
+    result = dict()
+
+    def _aux(
+            dict_obj: typing.Dict[str, typing.Any],
+            prefix: typing.Optional[str] = None,
+    ) -> typing.NoReturn:
+        prefix = prefix if prefix is not None else ""
+
+        for (key, value) in dict_obj.items():
+            path ="{prefix}.{key}".format(
+                prefix=prefix,
+                key=key,
+            ).strip(".")
+
+            if isinstance(value, dict):
+                _aux(
+                    dict_obj=value,
+                    prefix=path,
+                )
+
+            else:
+                result[path] = value
+
+    _aux(dict_obj=dict_obj)
+
+    return result
+
+
+def to_flat_dict(obj: typing.Any) -> typing.Dict[str, typing.Any]:
+    return dict_to_flat_dict(
+        dict_obj=to_dict(obj),
+    )
