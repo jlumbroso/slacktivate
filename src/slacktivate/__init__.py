a8e130497b2adb21dc619910b8fe153e11db4f48
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-09-25 00:29:13 -0400

    v0.2.5

--- src/slacktivate/__init__.py
+++ src/slacktivate/__init__.py
@@ -5,7 +5,7 @@ to assist in the provisioning of a Slack workspace, using
 both the Slack API and the Slack SCIM API.
 """
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
 
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split('.'))
