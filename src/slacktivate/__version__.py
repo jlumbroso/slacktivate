00ec578d0fe1b9d1abb4f205f642947af97ab0a3
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-08-22 18:50:29 -0400

    Updated `yaql`, v0.2.20

--- src/slacktivate/__version__.py
+++ src/slacktivate/__version__.py
@@ -1,3 +1,3 @@
 
-__version__ = "0.2.19"
+__version__ = "0.2.20"
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split('.'))
--- tests/test_slacktivate.py
+++ tests/test_slacktivate.py
@@ -2,4 +2,4 @@ from slacktivate import __version__
 
 
 def test_version():
-    assert __version__ == "0.2.19"
+    assert __version__ == "0.2.20"
